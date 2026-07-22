964b23dec9712503c64a6a82c62b768df4c17c22
Dev <165308292+DennyM55@users.noreply.github.com>
2026-07-22 11:10:01 +0530

    Add trailing zeroes solutions in Java and Python

---
+++ src/main/java/org/learn/P002_TrailingZeroesInFactorial.java
@@ -0,0 +1,24 @@
+package org.learn;
+
+// Problem 002: For an integer n, find the number of trailing zeroes in n!.
+
+public class P002_TrailingZeroesInFactorial {
+
+    static int trailingZeroes(int n) {
+        int zeroCount = 0;
+        long divisor = 5;
+
+        while (divisor <= n) {
+            int numberOfFives = (int) (n / divisor);
+            zeroCount = zeroCount + numberOfFives;
+            divisor = divisor * 5;
+        }
+
+        return zeroCount;
+    }
+
+    public static void main(String[] args) {
+        int n = 25;
+        System.out.println(trailingZeroes(n));
+    }
+}
---
+++ src/main/python/002_trailing_zeroes_in_factorial.py
@@ -0,0 +1,18 @@
+# Problem 002: For an integer n, find the number of trailing zeroes in n!.
+
+
+def trailing_zeroes(n: int) -> int:
+    zero_count = 0
+    divisor = 5
+
+    while divisor <= n:
+        number_of_fives = n // divisor
+        zero_count = zero_count + number_of_fives
+        divisor = divisor * 5
+
+    return zero_count
+
+
+if __name__ == "__main__":
+    n = 25
+    print(trailing_zeroes(n))
